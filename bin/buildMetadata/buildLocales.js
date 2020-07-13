186e7c16d3cbfeb083115b81589852bf4a7c752a
Jon McClure <jon.r.mcclure@gmail.com>
2020-07-13 19:37:42 +0100

    adds nation translation overrides

--- bin/buildMetadata/buildLocales.js
+++ bin/buildMetadata/buildLocales.js
@@ -1,5 +1,32 @@
 const fetch = require('node-fetch');
+const path = require('path');
+const fs = require('fs');
 const parseCSV = require('csv-parse/lib/sync');
+
+const DATA_DIR = path.join(__dirname, '../../data/');
+const enMetadataFile = fs.readFileSync(path.join(DATA_DIR, 'base_metadata.csv'), 'utf-8');
+const enMetadata = parseCSV(enMetadataFile, { columns: true, skip_empty_lines: true });
+const nameOverridesFile = fs.readFileSync(
+  path.join(DATA_DIR, 'translations/country_name_overrides.csv'), 'utf-8');
+const nameOverrides = parseCSV(nameOverridesFile, { columns: true, skip_empty_lines: true });
+
+const checkENOverrides = (isoAlpha2, locale, value) => {
+  if (locale !== 'en') return value;
+  const country = enMetadata.find(d => d.iso_alpha_2 === isoAlpha2);
+  if (!country) return value;
+  return country.name;
+};
+
+const checkCustomOverrides = (isoAlpha2, locale, value) => {
+  const country = nameOverrides.find(d => (d.isoAlpha2 === isoAlpha2) && (d.locale === locale));
+  if (!country) return value;
+  return country.name;
+};
+
+const checkOverrides = (isoAlpha2, locale, value) => {
+  // Basically, checkENOverrides => checkCustomOverrides
+  return checkCustomOverrides(isoAlpha2, locale, checkENOverrides(isoAlpha2, locale, value));
+};
 
 const fetchCSV = async(locale) => {
   const res = await fetch(`https://raw.githubusercontent.com/umpirsky/country-list/master/data/${locale}/country.csv`);
@@ -19,7 +46,7 @@ const buildLocales = async(locales) => {
       if (!(id in translations)) {
         translations[id] = {};
       }
-      translations[id][locale] = value;
+      translations[id][locale] = checkOverrides(id, locale, value);
     });
   }
   return translations;
